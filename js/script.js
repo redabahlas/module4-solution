3257bf9b44a59d0c7119c47a3e453e16222e80b1
redabahlas <34861597+redabahlas@users.noreply.github.com>
2022-03-22 23:10:09 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-
+var names = ["Reda", "yaakov", "Allen", "jay", "nurah", "Mohamed", "Aziz", "Karim", "Justin", "john", "Ahmed", "JOE"];
+for (var i = 0; i < names.length; i++) {
+	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+        console.log("Goodbye "+ names[i])
+	}
+	else{
+		console.log("Hello "+ names[i])
+	}
+}
