308cd85e9972d168574394367bf3b325ed377c76
redabahlas <34861597+redabahlas@users.noreply.github.com>
2022-03-23 12:43:45 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-var names = ["Reda", "yaakov", "Allen", "jay", "nurah", "Mohamed", "Aziz", "Karim", "Justin", "john", "Ahmed", "JOE"];
-for (var i = 0; i < names.length; i++) {
-	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
-        console.log("Goodbye "+ names[i])
+(function (window) {
+	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+	for (var i in names) {
+	  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+	  if (firstLetter == 'j') {
+		 window.byeSpeaker.speak(names[i]);
+	  } else {
+		 window.helloSpeaker.speak(names[i]);
+	  }
 	}
-	else{
-		console.log("Hello "+ names[i])
-	}
-}
+	})(window);
